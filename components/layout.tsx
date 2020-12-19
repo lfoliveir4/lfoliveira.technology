2f16db6175a35ef1e599747638caba883c8d003e
Luis Filipe Alves de Oliveira <luis.felipi@hotmail.com>
2020-12-19 14:18:28 -0300

    Commit on First change new version site

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-const name = 'Rodrigo Pauletti'
-export const siteTitle = 'Next.js Sample Website'
+const name = "Luis Filipe Alves de Oliveira";
+export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home = false }) {
   return (
@@ -35,23 +35,23 @@ export default function Layout({ children, home = false }) {
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
-            <>
+          <>
+            <Link href="/">
+              <a>
+                <img
+                  src="/images/profile.jpg"
+                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
               <Link href="/">
-                <a>
-                  <img
-                    src="/images/profile.jpg"
-                    className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                    alt={name}
-                  />
-                </a>
+                <a className={utilStyles.colorInherit}>{name}</a>
               </Link>
-              <h2 className={utilStyles.headingLg}>
-                <Link href="/">
-                  <a className={utilStyles.colorInherit}>{name}</a>
-                </Link>
-              </h2>
-            </>
-          )}
+            </h2>
+          </>
+        )}
       </header>
       <main>{children}</main>
       {!home && (
@@ -62,5 +62,5 @@ export default function Layout({ children, home = false }) {
         </div>
       )}
     </div>
-  )
+  );
 }
